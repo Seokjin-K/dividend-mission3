dbc2cf167987c5e85dd25b259b7ed02f86c4d287
seokjin <wlsdldos00@gmail.com>
2024-10-01 19:54:32 +0900

    Feat: Added Controller, Entity and Repository Dividend, Company Controller, Entity, Repository 생성 프로젝트 이름 수정 sample -> dividend

---
+++ src/main/java/com/dividend/persist/entity/DividendEntity.java
@@ -0,0 +1,25 @@
+package com.dividend.persist.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity(name = "DIVIDEND")
+@Getter
+@ToString
+@NoArgsConstructor
+public class DividendEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long companyId; // 어떤 회사의 배당금인지
+
+    private LocalDateTime date;
+
+    private String dividend;
+}
--- src/test/java/com/dividend/dayone/SampleApplicationTests.java
+++ src/test/java/com/dividend/dayone/SampleApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.sample;
+package com.dividend.dayone;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
